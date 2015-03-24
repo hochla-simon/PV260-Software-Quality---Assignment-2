13c7751bf4a6741ea3c9bd36b117e3aaa811fedd
Igor Orság <orsag@ics.muni.cz>
2015-03-24 21:48:51 +0100

    Collision detector

---
+++ Tron/src/tron/CollisionDetector.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tron;
+
+/**
+ *
+ * @author admin
+ */
+public class CollisionDetector {
+    public static boolean playerPairCollides(Player player1, Player player2){
+        return false;
+    }
+            
+}
